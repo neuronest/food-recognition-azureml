3f953c64ad06be182b7a39b1667f7ce03dae476d
Côme Arvis <come.arvis@neuronest.net>
2020-07-08 15:42:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-rm -rf ./env
 rm -rf .ipynb_checkpoints
 
-conda create -p .env python=3.6.2
-conda activate ./.env
+conda remove --name food_prediction --all
+conda create --name food_prediction python=3.6.2
+conda activate food_prediction
 conda install ipython
 conda install jupyter
 
